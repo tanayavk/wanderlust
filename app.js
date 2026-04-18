877be954c9d9d9ae05b6ad4728184e300e73d41e
Tanaya V Kakhandki <tanayavk15@gmail.com>
2026-04-18 14:48:12 +0530

    Fixed / route

--- app.js
+++ app.js
@@ -67,9 +67,6 @@ const sessionOptions={
     },
 };
 
-// app.get("/", (req,res)=>{
-//     res.send("Hi, I am root");
-// });
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -86,6 +83,10 @@ app.use((req, res, next)=>{
     res.locals.error = req.flash("error");
     res.locals.currUser=req.user;
     next();
+});
+
+app.get("/", (req, res) => {
+    res.redirect("/listings");
 });
 
 //LISTINGS
